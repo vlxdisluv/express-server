45c2985119c8e729d5fb586e0543b64aec14ea31
Vladislav Severin <michael23.jd@gmail.com>
2020-03-17 14:59:33 +0600

    Implement ts config

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
+const app = express();
+
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  // render the index template
+  res.render("index");
+});
+
+// start the express server
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
